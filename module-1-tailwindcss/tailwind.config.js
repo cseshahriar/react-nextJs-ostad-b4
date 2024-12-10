9b3d6638c47214da860dd8db936c99bc063bcbc7
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-12-10 12:31:35 +0600

    tailwind basics

--- module-1-tailwindcss/tailwind.config.js
+++ module-1-tailwindcss/tailwind.config.js
@@ -6,8 +6,31 @@ export default {
   ],
   darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff5722',
+        secondary: '#2196f3',
+        customcolor: {
+          100: '#e3f2fd',
+          500: '#2196f3',
+          900: '#01579b',
+        },
+      },
+      spacing: {
+        '1': '8px',
+        '2': '16px',
+        '3': '24px',
+      },
+      screens: {
+        'tablet': '640px',
+        'desktop': '1024px',
+        'big-desktop': '1280px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
 
