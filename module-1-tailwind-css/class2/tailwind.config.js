d1021b1aa8516078d03b9f77d2400eb9e60b83b9
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-01-21 22:42:44 +0600

    dark mode

---
+++ module-1-tailwind-css/class2/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  // darkMode: "media", // os
+  darkMode: "selector",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
