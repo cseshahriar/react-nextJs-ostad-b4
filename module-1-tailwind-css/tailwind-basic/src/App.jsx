1f148c7c9bbfcd0b51248d75b2ec7fc37cb29650
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-01-21 21:56:31 +0600

    class 1

--- module-1-tailwind-css/tailwind-basic/src/App.jsx
+++ module-1-tailwind-css/tailwind-basic/src/App.jsx
@@ -3,7 +3,17 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Utility First</h1>
+    {/* tailwind utility first framework */}
+      <div className='p-4 bg-gray-100'>
+        <h2 className='font-bold text-blue-600 mb-4'>We are learning tailwind</h2>
+        <button className='bg-blue-500 text-white p-4 rounded hover:bg-blue-900'>Click Me</button>
+      </div>
+
+      <div className='p-4 bg-gray-100'>
+        <h2 className='font-bold text-blue-600 mb-4'>Form</h2>
+        <input type='text' className='p-4 m-1' placeholder='Enter something' />
+        <button className='bg-blue-500 text-white p-4 rounded hover:bg-blue-900'>Submit</button>
+      </div>
     </>
   )
 }
