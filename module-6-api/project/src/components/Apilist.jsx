0d32f19a4a01129f6c63fe898eccd915e896b5ff
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-01-26 00:34:31 +0600

    fetch api

---
+++ module-6-api/project/src/components/Apilist.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState} from 'react'
+
+const Apilist = () => {
+    const [todos, setTodos] = useState(null);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => setTodos(json))
+    }, [])
+
+    return (
+        <div>
+            <h1>Fetch API</h1>
+            <table style={{width: '100%', background: 'gray', textAlign: "center", marginTop: "16px"}}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos?.map((item, index) => (
+                        <tr key={++index}>
+                            <td>{index}</td>
+                            <td>{item.title}</td>
+                            <td>{item.completed}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Apilist
--- module-6-api/project/src/pages/Home.jsx
+++ module-6-api/project/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Counter from '../components/Counter'
 import TodoList from '../components/TodoList'
-
+import Apilist from '../components/Apilist'
 
 const Home = () => {
   return (
     <div>
       {/* <Counter /> */}
-      <TodoList />
+      {/* <TodoList /> */}
+      <Apilist />
     </div>
   )
 }
